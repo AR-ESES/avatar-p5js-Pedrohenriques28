8e297f03cd3a6efdadce37bd5588400112fcd1d1
Pedro Lopes Henriques <pedrohenriques@MBP-de-Pedro-2.Home>
2024-03-28 22:41:52 +0000

    exercicio aula

--- sketch.js
+++ sketch.js
@@ -1,11 +1,39 @@
 function setup() {
-  createCanvas(1280, 1024);
-  background(153);
-  stroke(0);
-  strokeWeight(2);
-  fill(237, 28, 36);
-  circle(30, 30, 40);
-  line(30, 30, 1280 / 2, 1024 / 2);
+  createCanvas(1920, 1080);
 }
 
-function draw() {}
+function draw() {
+  background(20,400,30);
+  fill (75,155,175)
+  stroke(2)
+  rect(1920/2,1080/2,40,80)
+  
+   fill(213, 186, 152); 
+  stroke(0);
+  rect(925,350,70,80);
+  
+  
+  fill (75,155,175)
+  stroke(2)
+  rect(920,540,40,80)
+  
+  fill (50,50,10)
+  stroke(2)
+  rect(920,620,40,10)
+  
+  fill (50,50,10)
+  stroke(2)
+  rect(960,620,40,10)
+  
+  fill (157,25,25)
+  stroke(2)
+  rect(920,430,80,110)
+  
+  fill (157,25,25)
+  stroke(2)
+  rect(890,430,30,90)
+  
+  fill (157,25,25)
+  stroke(2)
+  rect(1000,430,30,90)
+}
